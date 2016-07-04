47f6aa1803105c583d69615f7bce8a1ff0bc3bd0
devesh <deveshasha.2@gmail.com>
2016-07-05 00:25:37 +0530

    removed dead links

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ app_name = 'polls'
 urlpatterns = [
 	
 	url(r'^$',views.test),
-	url(r'jtest/$', views.javaindex),
+	url(r'^jtest/$', views.javaindex),
 	url(r'jtest/result/$',views.javaresult),
 	url(r'phptest/$', views.phpindex),
 	url(r'phptest/result/$',views.phpresult),
